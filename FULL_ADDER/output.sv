9c435d2c80d8a07f7c28e3135e8dbdefa325be59
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-01-06 22:55:49 +0530

    output

---
+++ FULL_ADDER/output.sv
@@ -0,0 +1,36 @@
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;
+------------Generation of values------------
+a = 1 | b = 1 | c = 1 | sum = 0 | carry = 0
+------------Generation of values------------
+a = 1 | b = 0 | c = 1 | sum = 0 | carry = 0
+------------Generation of values------------
+a = 1 | b = 1 | c = 0 | sum = 0 | carry = 0
+------------Received values to driver------------
+a = 0 | b = 0 | c = 1 | sum = 0 | carry = 0
+------------Monitor Received------------
+a = 0 | b = 0 | c = 1 | sum = 1 | carry = 0
+------------Score board received------------
+a = 0 | b = 0 | c = 1 | sum = 1 | carry = 0
+~~~~~~~~~~~~~~~~~~~PASS~~~~~~~~~~~~~~~~~~~
+-------Transaction done----------
+
+------------Received values to driver------------
+a = 0 | b = 0 | c = 1 | sum = 0 | carry = 0
+------------Monitor Received------------
+a = 0 | b = 0 | c = 1 | sum = 1 | carry = 0
+------------Score board received------------
+a = 0 | b = 0 | c = 1 | sum = 1 | carry = 0
+~~~~~~~~~~~~~~~~~~~PASS~~~~~~~~~~~~~~~~~~~
+-------Transaction done----------
+
+------------Received values to driver------------
+a = 0 | b = 0 | c = 1 | sum = 0 | carry = 0
+------------Monitor Received------------
+a = 0 | b = 0 | c = 1 | sum = 1 | carry = 0
+------------Score board received------------
+a = 0 | b = 0 | c = 1 | sum = 1 | carry = 0
+~~~~~~~~~~~~~~~~~~~PASS~~~~~~~~~~~~~~~~~~~
+-------Transaction done----------
+
+$finish at simulation time                    3
