389db8c16a107ed449e118e3afe4bdf21f35bb59
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-01-07 23:45:55 +0530

    output

---
+++ D_FLIP_FLOP/output.sv
@@ -0,0 +1,153 @@
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;
+----------- DATA FROM GENERATOR -------------
+reset = 1 | d = 0 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM GENERATOR -------------
+reset = 0 | d = 1 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM GENERATOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM GENERATOR -------------
+reset = 0 | d = 1 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM GENERATOR -------------
+reset = 1 | d = 0 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM GENERATOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM GENERATOR -------------
+reset = 1 | d = 1 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM GENERATOR -------------
+reset = 1 | d = 1 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM GENERATOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM GENERATOR -------------
+reset = 1 | d = 1 | q = 0 | q_bar = 0
+Time = 0
+----------- DATA FROM DRIVER -------------
+reset = 1 | d = 0 | q = 0 | q_bar = 0
+Time = 5
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 11
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 11
+******************************
+            PASS              
+******************************
+----------- DATA FROM DRIVER -------------
+reset = 0 | d = 1 | q = 0 | q_bar = 0
+Time = 15
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 21
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 21
+******************************
+            PASS              
+******************************
+----------- DATA FROM DRIVER -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 25
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 31
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 31
+******************************
+            PASS              
+******************************
+----------- DATA FROM DRIVER -------------
+reset = 0 | d = 1 | q = 0 | q_bar = 0
+Time = 35
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 41
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 41
+******************************
+            PASS              
+******************************
+----------- DATA FROM DRIVER -------------
+reset = 1 | d = 0 | q = 0 | q_bar = 0
+Time = 45
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 51
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 51
+******************************
+            PASS              
+******************************
+----------- DATA FROM DRIVER -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 55
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 61
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 61
+******************************
+            PASS              
+******************************
+----------- DATA FROM DRIVER -------------
+reset = 1 | d = 1 | q = 0 | q_bar = 0
+Time = 65
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 71
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 71
+******************************
+            PASS              
+******************************
+----------- DATA FROM DRIVER -------------
+reset = 1 | d = 1 | q = 0 | q_bar = 0
+Time = 75
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 81
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 81
+******************************
+            PASS              
+******************************
+----------- DATA FROM DRIVER -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 85
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 91
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 91
+******************************
+            PASS              
+******************************
+----------- DATA FROM DRIVER -------------
+reset = 1 | d = 1 | q = 0 | q_bar = 0
+Time = 95
+----------- DATA FROM MONITOR -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 101
+----------- DATA FROM SCOREBOARD -------------
+reset = 0 | d = 0 | q = 0 | q_bar = 0
+Time = 101
+******************************
+            PASS              
+******************************
+$finish at simulation time                  101
