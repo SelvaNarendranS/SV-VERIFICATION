b8b95e93f33b0c31acb77c0fecf6ba737f97b9f7
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-01-09 23:04:39 +0530

    test

---
+++ COUNTER/09.test.sv
@@ -0,0 +1,11 @@
+// Counter verification - Test
+`include "environment.sv"
+
+program test #(parameter WIDTH)(intf intff);
+  environment #(WIDTH) env;
+  
+  initial begin
+    env = new(intff);
+    env.test_run();
+  end
+endprogram
