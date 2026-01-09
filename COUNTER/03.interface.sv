c8a3534a073b0cfffbda29e4d8b83af950cc3713
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-01-09 23:02:51 +0530

    interface

---
+++ COUNTER/03.interface.sv
@@ -0,0 +1,8 @@
+// Counter verification - interface
+interface intf #(parameter WIDTH = 8);
+  logic              clk;
+  logic              reset;
+  logic              enable;
+  logic              up_down;
+  logic [WIDTH -1:0] q;
+endinterface
