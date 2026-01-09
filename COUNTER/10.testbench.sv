48609d1614e3f417cd968c10d20785d2387a62c7
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-01-09 23:04:58 +0530

    testbench

---
+++ COUNTER/10.testbench.sv
@@ -0,0 +1,30 @@
+// Counter verification - Testbench
+`include "test.sv"
+`include "interface.sv"
+
+module testbench;
+  parameter WIDTH = 4;				// counter size
+  
+  intf #(WIDTH) intff();			// interface handle
+  test #(WIDTH) tst(intff);			// passing interface to the test
+  
+  // instantation
+  counter #(.WIDTH(WIDTH)) 
+  dut(
+    .clk(intff.clk),
+    .reset(intff.reset),
+    .enable(intff.enable),
+    .up_down(intff.up_down),
+    .q(intff.q)
+  );
+  
+  initial intff.clk = 1;
+  initial forever #5 intff.clk = ~intff.clk;
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    
+    #10000 $finish;
+  end
+endmodule
