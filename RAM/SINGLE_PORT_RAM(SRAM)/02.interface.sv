06f322bc628bbdea1dec18472c7f9760b3a159d7
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-02-22 23:21:47 +0530

    interface

---
+++ RAM/SINGLE_PORT_RAM(SRAM)/02.interface.sv
@@ -0,0 +1,31 @@
+// Single port ram - interface
+interface intf #(parameter WIDTH = 8, 
+                 parameter DEPTH = 4);		// paramaterized interface
+
+  // system
+  logic						clk;
+  
+  // input ports
+  logic 					en;
+  logic  				    wr_en;			// write enable
+  logic [WIDTH-1:0]			data_in;
+  logic [$clog2(DEPTH)-1:0] addr;
+  
+  // output ports
+  logic [WIDTH-1:0]			data_out;
+  
+  // clocking block
+  
+  // clocking for driving stimuli to dut
+  clocking cb_driver @(negedge clk);		
+    output #2 en, wr_en, data_in, addr;
+    input #1 data_out;
+  endclocking
+  
+  // clocking for sampling result from dut
+  clocking cb_monitor @(posedge clk);
+    default input #0;						// #0 to sample the input exactly at posedge to get imediate 
+    input en, wr_en, data_in, addr, data_out;
+  endclocking
+  
+endinterface
