ae58c86fb6816b2ec6a2a77bbe37220aaec6dd3f
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-02-24 23:32:19 +0530

    agent

--- RAM/SINGLE_PORT_RAM(SRAM)/07.agent.sv
+++ RAM/SINGLE_PORT_RAM(SRAM)/07.agent.sv
@@ -12,20 +12,22 @@ class agent #(WIDTH, DEPTH);
   
   mailbox gen2drv;
   mailbox mon2scb;
+  mailbox mon2cov;
   
   virtual intf #(WIDTH, DEPTH) vintf;
   
   event next;
   
   // function new constructor
-  function new(mailbox mon2scb, virtual intf #(WIDTH, DEPTH) vintf, event next);
+  function new(mailbox mon2scb, mailbox mon2cov, virtual intf #(WIDTH, DEPTH) vintf, event next);
     this.vintf   = vintf;
     this.mon2scb = mon2scb;
+    this.mon2cov = mon2cov;
     
     gen2drv = new();
     gen     = new(gen2drv);
     drv		= new(gen2drv, vintf);
-    mon		= new(mon2scb, vintf);
+    mon		= new(mon2scb, mon2cov, vintf);
     
     gen.next = next;
   endfunction
