dcd1c1fc9b7166078ff9853020ca3cb1f9717f8f
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-02-24 23:39:20 +0530

    output

---
+++ RAM/SINGLE_PORT_RAM(SRAM)/13.output.sv
@@ -0,0 +1,256 @@
+# run -all
+# ----- GENERATOR STARTED ------
+# ------------ Disable condition -----------
+# ----------------  DISABLE GENERATED VALUES  ------------------
+# Time = 0 | en = 0 | wr_en = 1 | addr = 6 | data_in = 9 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 5 | en = 0 | wr_en = 1 | addr = 6 | data_in = 9 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 10 | en = 0 | wr_en = 1 | addr = 6 | data_in = 9 | data_out = 0
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 10 | en = 0 | wr_en = 1 | addr = 6 | data_in = 9 | data_out = 0
+# 1
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 15.62%
+# 
+# ------------ write condition -----------
+# ---------------- WRITE GENERATED VALUES  ------------------
+# Time = 10 | en = 1 | wr_en = 1 | addr = 3 | data_in = 6 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 15 | en = 1 | wr_en = 1 | addr = 3 | data_in = 6 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 20 | en = 1 | wr_en = 1 | addr = 3 | data_in = 6 | data_out = 0
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 20 | en = 1 | wr_en = 1 | addr = 3 | data_in = 6 | data_out = 0
+# 2
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 45.31%
+# 
+# ------------ write condition -----------
+# ---------------- WRITE GENERATED VALUES  ------------------
+# Time = 20 | en = 1 | wr_en = 1 | addr = 5 | data_in = 6 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 25 | en = 1 | wr_en = 1 | addr = 5 | data_in = 6 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 30 | en = 1 | wr_en = 1 | addr = 5 | data_in = 6 | data_out = 0
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 30 | en = 1 | wr_en = 1 | addr = 5 | data_in = 6 | data_out = 0
+# 3
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 50.00%
+# 
+# ------------ write condition -----------
+# ---------------- WRITE GENERATED VALUES  ------------------
+# Time = 30 | en = 1 | wr_en = 1 | addr = 7 | data_in = c | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 35 | en = 1 | wr_en = 1 | addr = 7 | data_in = c | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 40 | en = 1 | wr_en = 1 | addr = 7 | data_in = c | data_out = 0
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 40 | en = 1 | wr_en = 1 | addr = 7 | data_in = c | data_out = 0
+# 4
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 54.69%
+# 
+# ------------ write condition -----------
+# ---------------- WRITE GENERATED VALUES  ------------------
+# Time = 40 | en = 1 | wr_en = 1 | addr = 0 | data_in = 8 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 45 | en = 1 | wr_en = 1 | addr = 0 | data_in = 8 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 50 | en = 1 | wr_en = 1 | addr = 0 | data_in = 8 | data_out = 0
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 50 | en = 1 | wr_en = 1 | addr = 0 | data_in = 8 | data_out = 0
+# 5
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 59.38%
+# 
+# ------------ write condition -----------
+# ---------------- WRITE GENERATED VALUES  ------------------
+# Time = 50 | en = 1 | wr_en = 1 | addr = 1 | data_in = 7 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 55 | en = 1 | wr_en = 1 | addr = 1 | data_in = 7 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 60 | en = 1 | wr_en = 1 | addr = 1 | data_in = 7 | data_out = 0
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 60 | en = 1 | wr_en = 1 | addr = 1 | data_in = 7 | data_out = 0
+# 6
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 64.06%
+# 
+# ------------ write condition -----------
+# ---------------- WRITE GENERATED VALUES  ------------------
+# Time = 60 | en = 1 | wr_en = 1 | addr = 4 | data_in = a | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 65 | en = 1 | wr_en = 1 | addr = 4 | data_in = a | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 70 | en = 1 | wr_en = 1 | addr = 4 | data_in = a | data_out = 0
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 70 | en = 1 | wr_en = 1 | addr = 4 | data_in = a | data_out = 0
+# 7
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 68.75%
+# 
+# ------------ write condition -----------
+# ---------------- WRITE GENERATED VALUES  ------------------
+# Time = 70 | en = 1 | wr_en = 1 | addr = 2 | data_in = f | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 75 | en = 1 | wr_en = 1 | addr = 2 | data_in = f | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 80 | en = 1 | wr_en = 1 | addr = 2 | data_in = f | data_out = 0
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 80 | en = 1 | wr_en = 1 | addr = 2 | data_in = f | data_out = 0
+# 8
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 73.44%
+# 
+# ------------ read condition -----------
+# ---------------- READ GENERATED VALUES  ------------------
+# Time = 80 | en = 1 | wr_en = 0 | addr = 6 | data_in = 0 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 85 | en = 1 | wr_en = 0 | addr = 6 | data_in = 0 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 90 | en = 1 | wr_en = 0 | addr = 6 | data_in = 0 | data_out = 0
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 90 | en = 1 | wr_en = 0 | addr = 6 | data_in = 0 | data_out = 0
+# 9
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 87.50%
+# 
+# ------------ read condition -----------
+# ---------------- READ GENERATED VALUES  ------------------
+# Time = 90 | en = 1 | wr_en = 0 | addr = 7 | data_in = 0 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 95 | en = 1 | wr_en = 0 | addr = 7 | data_in = 0 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 100 | en = 1 | wr_en = 0 | addr = 7 | data_in = 0 | data_out = c
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 100 | en = 1 | wr_en = 0 | addr = 7 | data_in = 0 | data_out = c
+# 10
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 89.06%
+# 
+# ------------ read condition -----------
+# ---------------- READ GENERATED VALUES  ------------------
+# Time = 100 | en = 1 | wr_en = 0 | addr = 2 | data_in = 0 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 105 | en = 1 | wr_en = 0 | addr = 2 | data_in = 0 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 110 | en = 1 | wr_en = 0 | addr = 2 | data_in = 0 | data_out = f
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 110 | en = 1 | wr_en = 0 | addr = 2 | data_in = 0 | data_out = f
+# 11
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 90.62%
+# 
+# ------------ read condition -----------
+# ---------------- READ GENERATED VALUES  ------------------
+# Time = 110 | en = 1 | wr_en = 0 | addr = 3 | data_in = 0 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 115 | en = 1 | wr_en = 0 | addr = 3 | data_in = 0 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 120 | en = 1 | wr_en = 0 | addr = 3 | data_in = 0 | data_out = 6
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 120 | en = 1 | wr_en = 0 | addr = 3 | data_in = 0 | data_out = 6
+# 12
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 92.19%
+# 
+# ------------ read condition -----------
+# ---------------- READ GENERATED VALUES  ------------------
+# Time = 120 | en = 1 | wr_en = 0 | addr = 4 | data_in = 0 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 125 | en = 1 | wr_en = 0 | addr = 4 | data_in = 0 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 130 | en = 1 | wr_en = 0 | addr = 4 | data_in = 0 | data_out = a
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 130 | en = 1 | wr_en = 0 | addr = 4 | data_in = 0 | data_out = a
+# 13
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 93.75%
+# 
+# ------------ read condition -----------
+# ---------------- READ GENERATED VALUES  ------------------
+# Time = 130 | en = 1 | wr_en = 0 | addr = 5 | data_in = 0 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 135 | en = 1 | wr_en = 0 | addr = 5 | data_in = 0 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 140 | en = 1 | wr_en = 0 | addr = 5 | data_in = 0 | data_out = 6
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 140 | en = 1 | wr_en = 0 | addr = 5 | data_in = 0 | data_out = 6
+# 14
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 95.31%
+# 
+# ------------ read condition -----------
+# ---------------- READ GENERATED VALUES  ------------------
+# Time = 140 | en = 1 | wr_en = 0 | addr = 0 | data_in = 0 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 145 | en = 1 | wr_en = 0 | addr = 0 | data_in = 0 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 150 | en = 1 | wr_en = 0 | addr = 0 | data_in = 0 | data_out = 8
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 150 | en = 1 | wr_en = 0 | addr = 0 | data_in = 0 | data_out = 8
+# 15
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 96.88%
+# 
+# ------------ read condition -----------
+# ---------------- READ GENERATED VALUES  ------------------
+# Time = 150 | en = 1 | wr_en = 0 | addr = 1 | data_in = 0 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 155 | en = 1 | wr_en = 0 | addr = 1 | data_in = 0 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 160 | en = 1 | wr_en = 0 | addr = 1 | data_in = 0 | data_out = 7
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 160 | en = 1 | wr_en = 0 | addr = 1 | data_in = 0 | data_out = 7
+# 16
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 98.44%
+# 
+# ------------ read condition -----------
+# ---------------- READ GENERATED VALUES  ------------------
+# Time = 160 | en = 1 | wr_en = 0 | addr = 2 | data_in = 0 | data_out = 0
+# ----------------  DRIVER VALUES  ------------------
+# Time = 165 | en = 1 | wr_en = 0 | addr = 2 | data_in = 0 | data_out = 0
+# ---------------- MONITOR VALUES ------------------
+# Time = 170 | en = 1 | wr_en = 0 | addr = 2 | data_in = 0 | data_out = f
+# ---------------- SCOREBOARD VALUE ------------------
+# Time = 170 | en = 1 | wr_en = 0 | addr = 2 | data_in = 0 | data_out = f
+# 17
+# --------------------------------------------
+#                     PASS
+# --------------------------------------------
+# The Functional coverage = 98.44%
