ef41bc0ceb606beff2f7ad2777dd8e81f5d5ec2c
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-02-27 00:03:03 +0530

    output

---
+++ RAM/DUAL_PORT_RAM(DRAM)/13.output.sv
@@ -0,0 +1,279 @@
+Contains Synopsys proprietary information.
+Compiler version X-2025.06-SP1_Full64; Runtime version X-2025.06-SP1_Full64;
+ -------------------------- DUAL PORT RAM MODE0(read first) ------------------------------- 
+----- GENERATOR STARTED ------
+------------ Disable condition -----------
+----------------  DISABLE GENERATED VALUES  ------------------
+Time = 20000 | en = 0 | a_wr = 0 | a_addr = 7 | a_data_in = 0 | b_wr = 1 | b_addr = 5 | b_data_in = 1 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 20000 | en = 0 | a_wr = 0 | a_addr = 7 | a_data_in = 0 | b_wr = 1 | b_addr = 5 | b_data_in = 1 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 25000 | en = 0 | a_wr = 0 | a_addr = 7 | a_data_in = 0 | b_wr = 1 | b_addr = 5 | b_data_in = 1 | a_data_out = 0 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 25000 | en = 0 | a_wr = 0 | a_addr = 7 | a_data_in = 0 | b_wr = 1 | b_addr = 5 | b_data_in = 1 | a_data_out = 0 | b_data_out = 0
+1
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 16.67%
+
+------------ PORT-A write condition -----------
+---------------- PORT-A WRITE GENERATED VALUES  ------------------
+Time = 25000 | en = 1 | a_wr = 1 | a_addr = 5 | a_data_in = 15 | b_wr = 0 | b_addr = 7 | b_data_in = 5 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 30000 | en = 1 | a_wr = 1 | a_addr = 5 | a_data_in = 15 | b_wr = 0 | b_addr = 7 | b_data_in = 5 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 35000 | en = 1 | a_wr = 1 | a_addr = 5 | a_data_in = 15 | b_wr = 0 | b_addr = 7 | b_data_in = 5 | a_data_out = 0 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 35000 | en = 1 | a_wr = 1 | a_addr = 5 | a_data_in = 15 | b_wr = 0 | b_addr = 7 | b_data_in = 5 | a_data_out = 0 | b_data_out = 0
+2
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 66.67%
+
+------------ PORT-A write condition -----------
+---------------- PORT-A WRITE GENERATED VALUES  ------------------
+Time = 35000 | en = 1 | a_wr = 1 | a_addr = 6 | a_data_in = 14 | b_wr = 0 | b_addr = 6 | b_data_in = 6 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 40000 | en = 1 | a_wr = 1 | a_addr = 6 | a_data_in = 14 | b_wr = 0 | b_addr = 6 | b_data_in = 6 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 45000 | en = 1 | a_wr = 1 | a_addr = 6 | a_data_in = 14 | b_wr = 0 | b_addr = 6 | b_data_in = 6 | a_data_out = 0 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 45000 | en = 1 | a_wr = 1 | a_addr = 6 | a_data_in = 14 | b_wr = 0 | b_addr = 6 | b_data_in = 6 | a_data_out = 0 | b_data_out = 0
+3
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 66.67%
+
+------------ PORT-A write condition -----------
+---------------- PORT-A WRITE GENERATED VALUES  ------------------
+Time = 45000 | en = 1 | a_wr = 1 | a_addr = 4 | a_data_in = 12 | b_wr = 0 | b_addr = 3 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 50000 | en = 1 | a_wr = 1 | a_addr = 4 | a_data_in = 12 | b_wr = 0 | b_addr = 3 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 55000 | en = 1 | a_wr = 1 | a_addr = 4 | a_data_in = 12 | b_wr = 0 | b_addr = 3 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 55000 | en = 1 | a_wr = 1 | a_addr = 4 | a_data_in = 12 | b_wr = 0 | b_addr = 3 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+4
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 66.67%
+
+------------ PORT-B write condition -----------
+---------------- PORT-B WRITE GENERATED VALUES  ------------------
+Time = 55000 | en = 1 | a_wr = 0 | a_addr = 3 | a_data_in = 6 | b_wr = 1 | b_addr = 4 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 60000 | en = 1 | a_wr = 0 | a_addr = 3 | a_data_in = 6 | b_wr = 1 | b_addr = 4 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 65000 | en = 1 | a_wr = 0 | a_addr = 3 | a_data_in = 6 | b_wr = 1 | b_addr = 4 | b_data_in = 0 | a_data_out = 0 | b_data_out = 12
+---------------- SCOREBOARD VALUE ------------------
+Time = 65000 | en = 1 | a_wr = 0 | a_addr = 3 | a_data_in = 6 | b_wr = 1 | b_addr = 4 | b_data_in = 0 | a_data_out = 0 | b_data_out = 12
+5
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 75.00%
+
+------------ PORT-B write condition -----------
+---------------- PORT-B WRITE GENERATED VALUES  ------------------
+Time = 65000 | en = 1 | a_wr = 0 | a_addr = 1 | a_data_in = 11 | b_wr = 1 | b_addr = 0 | b_data_in = 15 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 70000 | en = 1 | a_wr = 0 | a_addr = 1 | a_data_in = 11 | b_wr = 1 | b_addr = 0 | b_data_in = 15 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 75000 | en = 1 | a_wr = 0 | a_addr = 1 | a_data_in = 11 | b_wr = 1 | b_addr = 0 | b_data_in = 15 | a_data_out = 0 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 75000 | en = 1 | a_wr = 0 | a_addr = 1 | a_data_in = 11 | b_wr = 1 | b_addr = 0 | b_data_in = 15 | a_data_out = 0 | b_data_out = 0
+6
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 75.00%
+
+------------ PORT-B write condition -----------
+---------------- PORT-B WRITE GENERATED VALUES  ------------------
+Time = 75000 | en = 1 | a_wr = 0 | a_addr = 0 | a_data_in = 8 | b_wr = 1 | b_addr = 1 | b_data_in = 15 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 80000 | en = 1 | a_wr = 0 | a_addr = 0 | a_data_in = 8 | b_wr = 1 | b_addr = 1 | b_data_in = 15 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 85000 | en = 1 | a_wr = 0 | a_addr = 0 | a_data_in = 8 | b_wr = 1 | b_addr = 1 | b_data_in = 15 | a_data_out = 15 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 85000 | en = 1 | a_wr = 0 | a_addr = 0 | a_data_in = 8 | b_wr = 1 | b_addr = 1 | b_data_in = 15 | a_data_out = 15 | b_data_out = 0
+7
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 75.00%
+
+------------ PORT-B write condition -----------
+---------------- PORT-B WRITE GENERATED VALUES  ------------------
+Time = 85000 | en = 1 | a_wr = 0 | a_addr = 2 | a_data_in = 14 | b_wr = 1 | b_addr = 2 | b_data_in = 7 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 90000 | en = 1 | a_wr = 0 | a_addr = 2 | a_data_in = 14 | b_wr = 1 | b_addr = 2 | b_data_in = 7 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 95000 | en = 1 | a_wr = 0 | a_addr = 2 | a_data_in = 14 | b_wr = 1 | b_addr = 2 | b_data_in = 7 | a_data_out = 0 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 95000 | en = 1 | a_wr = 0 | a_addr = 2 | a_data_in = 14 | b_wr = 1 | b_addr = 2 | b_data_in = 7 | a_data_out = 0 | b_data_out = 0
+8
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 75.00%
+
+------------ PORT-B read condition -----------
+---------------- PORT-B READ GENERATED VALUES  ------------------
+Time = 95000 | en = 1 | a_wr = 0 | a_addr = 4 | a_data_in = 3 | b_wr = 0 | b_addr = 6 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 100000 | en = 1 | a_wr = 0 | a_addr = 4 | a_data_in = 3 | b_wr = 0 | b_addr = 6 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 105000 | en = 1 | a_wr = 0 | a_addr = 4 | a_data_in = 3 | b_wr = 0 | b_addr = 6 | b_data_in = 0 | a_data_out = 0 | b_data_out = 14
+---------------- SCOREBOARD VALUE ------------------
+Time = 105000 | en = 1 | a_wr = 0 | a_addr = 4 | a_data_in = 3 | b_wr = 0 | b_addr = 6 | b_data_in = 0 | a_data_out = 0 | b_data_out = 14
+9
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 83.33%
+
+------------ PORT-B read condition -----------
+---------------- PORT-B READ GENERATED VALUES  ------------------
+Time = 105000 | en = 1 | a_wr = 0 | a_addr = 1 | a_data_in = 11 | b_wr = 0 | b_addr = 7 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 110000 | en = 1 | a_wr = 0 | a_addr = 1 | a_data_in = 11 | b_wr = 0 | b_addr = 7 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 115000 | en = 1 | a_wr = 0 | a_addr = 1 | a_data_in = 11 | b_wr = 0 | b_addr = 7 | b_data_in = 0 | a_data_out = 15 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 115000 | en = 1 | a_wr = 0 | a_addr = 1 | a_data_in = 11 | b_wr = 0 | b_addr = 7 | b_data_in = 0 | a_data_out = 15 | b_data_out = 0
+10
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 83.33%
+
+------------ PORT-B read condition -----------
+---------------- PORT-B READ GENERATED VALUES  ------------------
+Time = 115000 | en = 1 | a_wr = 1 | a_addr = 2 | a_data_in = 2 | b_wr = 0 | b_addr = 3 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 120000 | en = 1 | a_wr = 1 | a_addr = 2 | a_data_in = 2 | b_wr = 0 | b_addr = 3 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 125000 | en = 1 | a_wr = 1 | a_addr = 2 | a_data_in = 2 | b_wr = 0 | b_addr = 3 | b_data_in = 0 | a_data_out = 7 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 125000 | en = 1 | a_wr = 1 | a_addr = 2 | a_data_in = 2 | b_wr = 0 | b_addr = 3 | b_data_in = 0 | a_data_out = 7 | b_data_out = 0
+11
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 83.33%
+
+------------ PORT-B read condition -----------
+---------------- PORT-B READ GENERATED VALUES  ------------------
+Time = 125000 | en = 1 | a_wr = 1 | a_addr = 5 | a_data_in = 5 | b_wr = 0 | b_addr = 4 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 130000 | en = 1 | a_wr = 1 | a_addr = 5 | a_data_in = 5 | b_wr = 0 | b_addr = 4 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 135000 | en = 1 | a_wr = 1 | a_addr = 5 | a_data_in = 5 | b_wr = 0 | b_addr = 4 | b_data_in = 0 | a_data_out = 15 | b_data_out = 0
+---------------- SCOREBOARD VALUE ------------------
+Time = 135000 | en = 1 | a_wr = 1 | a_addr = 5 | a_data_in = 5 | b_wr = 0 | b_addr = 4 | b_data_in = 0 | a_data_out = 15 | b_data_out = 0
+12
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 83.33%
+
+------------ PORT-B read condition -----------
+---------------- PORT-B READ GENERATED VALUES  ------------------
+Time = 135000 | en = 1 | a_wr = 1 | a_addr = 7 | a_data_in = 13 | b_wr = 0 | b_addr = 2 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 140000 | en = 1 | a_wr = 1 | a_addr = 7 | a_data_in = 13 | b_wr = 0 | b_addr = 2 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 145000 | en = 1 | a_wr = 1 | a_addr = 7 | a_data_in = 13 | b_wr = 0 | b_addr = 2 | b_data_in = 0 | a_data_out = 0 | b_data_out = 2
+---------------- SCOREBOARD VALUE ------------------
+Time = 145000 | en = 1 | a_wr = 1 | a_addr = 7 | a_data_in = 13 | b_wr = 0 | b_addr = 2 | b_data_in = 0 | a_data_out = 0 | b_data_out = 2
+13
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 83.33%
+
+------------ PORT-B read condition -----------
+---------------- PORT-B READ GENERATED VALUES  ------------------
+Time = 145000 | en = 1 | a_wr = 1 | a_addr = 3 | a_data_in = 15 | b_wr = 0 | b_addr = 1 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 150000 | en = 1 | a_wr = 1 | a_addr = 3 | a_data_in = 15 | b_wr = 0 | b_addr = 1 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 155000 | en = 1 | a_wr = 1 | a_addr = 3 | a_data_in = 15 | b_wr = 0 | b_addr = 1 | b_data_in = 0 | a_data_out = 0 | b_data_out = 15
+---------------- SCOREBOARD VALUE ------------------
+Time = 155000 | en = 1 | a_wr = 1 | a_addr = 3 | a_data_in = 15 | b_wr = 0 | b_addr = 1 | b_data_in = 0 | a_data_out = 0 | b_data_out = 15
+14
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 83.33%
+
+------------ PORT-B read condition -----------
+---------------- PORT-B READ GENERATED VALUES  ------------------
+Time = 155000 | en = 1 | a_wr = 1 | a_addr = 0 | a_data_in = 15 | b_wr = 0 | b_addr = 5 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 160000 | en = 1 | a_wr = 1 | a_addr = 0 | a_data_in = 15 | b_wr = 0 | b_addr = 5 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 165000 | en = 1 | a_wr = 1 | a_addr = 0 | a_data_in = 15 | b_wr = 0 | b_addr = 5 | b_data_in = 0 | a_data_out = 15 | b_data_out = 5
+---------------- SCOREBOARD VALUE ------------------
+Time = 165000 | en = 1 | a_wr = 1 | a_addr = 0 | a_data_in = 15 | b_wr = 0 | b_addr = 5 | b_data_in = 0 | a_data_out = 15 | b_data_out = 5
+15
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 83.33%
+
+------------ PORT-B read condition -----------
+---------------- PORT-B READ GENERATED VALUES  ------------------
+Time = 165000 | en = 1 | a_wr = 1 | a_addr = 6 | a_data_in = 14 | b_wr = 0 | b_addr = 0 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 170000 | en = 1 | a_wr = 1 | a_addr = 6 | a_data_in = 14 | b_wr = 0 | b_addr = 0 | b_data_in = 0 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 175000 | en = 1 | a_wr = 1 | a_addr = 6 | a_data_in = 14 | b_wr = 0 | b_addr = 0 | b_data_in = 0 | a_data_out = 14 | b_data_out = 15
+---------------- SCOREBOARD VALUE ------------------
+Time = 175000 | en = 1 | a_wr = 1 | a_addr = 6 | a_data_in = 14 | b_wr = 0 | b_addr = 0 | b_data_in = 0 | a_data_out = 14 | b_data_out = 15
+16
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 83.33%
+
+------------ priority condition write -----------
+----------------  PARITY CHECKING WRITE GENERATED VALUES  ------------------
+Time = 175000 | en = 1 | a_wr = 1 | a_addr = 2 | a_data_in = 5 | b_wr = 1 | b_addr = 2 | b_data_in = 11 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 180000 | en = 1 | a_wr = 1 | a_addr = 2 | a_data_in = 5 | b_wr = 1 | b_addr = 2 | b_data_in = 11 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 185000 | en = 1 | a_wr = 1 | a_addr = 2 | a_data_in = 5 | b_wr = 1 | b_addr = 2 | b_data_in = 11 | a_data_out = 2 | b_data_out = 2
+---------------- SCOREBOARD VALUE ------------------
+Time = 185000 | en = 1 | a_wr = 1 | a_addr = 2 | a_data_in = 5 | b_wr = 1 | b_addr = 2 | b_data_in = 11 | a_data_out = 2 | b_data_out = 2
+17
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 100.00%
+
+
+------------ priority condition read -----------
+----------------  PARITY CHECKING READ GENERATED VALUES  ------------------
+Time = 185000 | en = 1 | a_wr = 0 | a_addr = 2 | a_data_in = 14 | b_wr = 0 | b_addr = 2 | b_data_in = 6 | a_data_out = 0 | b_data_out = 0
+---------------- RECEIVED VALUES TO DRIVER ------------------
+Time = 190000 | en = 1 | a_wr = 0 | a_addr = 2 | a_data_in = 14 | b_wr = 0 | b_addr = 2 | b_data_in = 6 | a_data_out = 0 | b_data_out = 0
+---------------- SAMPLED VALUES IN MONITOR ------------------
+Time = 195000 | en = 1 | a_wr = 0 | a_addr = 2 | a_data_in = 14 | b_wr = 0 | b_addr = 2 | b_data_in = 6 | a_data_out = 5 | b_data_out = 5
+---------------- SCOREBOARD VALUE ------------------
+Time = 195000 | en = 1 | a_wr = 0 | a_addr = 2 | a_data_in = 14 | b_wr = 0 | b_addr = 2 | b_data_in = 6 | a_data_out = 5 | b_data_out = 5
+18
+--------------------------------------------
+                    PASS
+--------------------------------------------
+The Functional coverage = 100.00%
+
+The TOTAL Functional coverage = 100.00%
+
+$finish called from file "environment.sv",
+$finish at simulation time               195000
